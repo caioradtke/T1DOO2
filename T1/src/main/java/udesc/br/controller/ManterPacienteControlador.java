9da0f381bf31383b0e34bb52dfc229a012252a04
Caio Radtke <caioradtke19@gmail.com>
2026-06-03 20:18:17 -0300

    feat: Package Exception com ProdutoException, ManterProdutoVisao, ManterProdutoControlador refactor: classes de repositorio e PacienteControlador apagado

---
+++ T1/src/main/java/udesc/br/controller/ManterPacienteControlador.java
@@ -0,0 +1,56 @@
+package udesc.br.controller;
+
+import exception.PacienteException;
+import udesc.br.model.Paciente;
+import udesc.br.repository.PacienteRepositorio;
+import vision.ManterPacienteVisao;
+
+
+public class ManterPacienteControlador {
+
+    private ManterPacienteVisao visao;
+    private Paciente modelo;
+    private PacienteRepositorio pacienteRepositorio;
+
+    public ManterPacienteControlador(ManterPacienteVisao visao, PacienteRepositorio produtoRepositorio){
+        this.visao = visao;
+        this.pacienteRepositorio = produtoRepositorio;
+        initTela();
+    }
+
+    public void initTela(){
+        adicionarAcoes();
+        visao.apresentarPacientes(pacienteRepositorio.buscarTodosPacientes());
+        this.visao.apresentarTela();
+    }
+
+    public void adicionarAcoes(){
+        
+        visao.adicionarAcaoBtnSalvar(e -> salvarPaciente());
+    }
+
+
+
+    //Fluxo principal
+    public void salvarPaciente(){
+        try {
+            String nome = visao.getPacienteNome();
+            String cpf = visao.getPacienteCpf();
+            String telefone = visao.getPacienteTelefone();
+            Double altura = visao.getPacienteAltura();
+            Double peso = visao.getPacienteAltura();
+
+            modelo = new Paciente(nome, cpf, peso, altura, telefone);
+
+            pacienteRepositorio.salvarPaciente(modelo);
+
+            visao.apresentarPacientes(pacienteRepositorio.buscarTodosPacientes());
+            visao.apresentarMensagem(modelo.toString() + " Salvo com sucesso");
+            visao.limparTela();
+
+        } catch (PacienteException ex){
+            visao.apresentarMensagem(ex.getMessage());
+        }
+
+    }
+}
